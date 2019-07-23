c4da6c2c27a401bbfb1e373c38c0ac5cfdd29ebf
Hilton Santos <jhilton930@gmail.com>
2019-07-23 15:07:56 -0300

    Retornando Fabio01-02

--- Atividade_Fabio_01/fabio01_02_minutos_totais.py
+++ Atividade_Fabio_01/fabio01_02_minutos_totais.py
@@ -7,12 +7,3 @@ calculo_minutos = (horas * 60) + minutos
 
 # saida
 print('O valor total em minutos sera de: {} minutos.'.format(calculo_minutos))
-
-
-<<<<<<< HEAD
-=======
-print('Testando Git')
-
-
-
->>>>>>> d88517293dd8b21b744c8a43e929933babe344bc
